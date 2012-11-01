ebef4e7f6fb5c9a0d5bd9165e1f5b97d1e73d0fe
Heitor Augusto Schmitt <heitorschmitt@gmail.com>
2012-11-01 20:03:19 -0200

    Transform + Color
    Transform + Color working

---
+++ iV-ART/GlobalVar.h
@@ -0,0 +1,13 @@
+//
+//  GlobalVar.h
+//  iV-ART
+//
+//  Created by Heitor Augusto Schmitt on 11/1/12.
+//  Copyright (c) 2012 Heitor Augusto Schmitt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GlobalVar : NSObject
+
+@end
---
+++ iV-ART/GlobalVar.m
@@ -0,0 +1,13 @@
+//
+//  GlobalVar.m
+//  iV-ART
+//
+//  Created by Heitor Augusto Schmitt on 11/1/12.
+//  Copyright (c) 2012 Heitor Augusto Schmitt. All rights reserved.
+//
+
+#import "GlobalVar.h"
+
+@implementation GlobalVar
+
+@end
